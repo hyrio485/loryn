8f968e599a08d24cc6aabe35cd5db925c8ceb5d7
wangchen3 <wangchen3@eastmoney.com>
2025-04-01 09:49:20 +0800

    feat: 新增JsonObjectSqlType

--- loryn-core/src/main/java/top/loryn/support/external/JsonSqlType.kt
+++ loryn-core/src/main/java/top/loryn/support/external/JsonSqlType.kt
@@ -1,6 +1,7 @@
 package top.loryn.support.external
 
 import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.node.ObjectNode
 import top.loryn.schema.Column
 import top.loryn.schema.Table
 import top.loryn.support.SqlType
@@ -23,4 +24,13 @@ object JsonSqlType : SqlType<JsonNode>(JDBCType.VARCHAR, JsonNode::class.java) {
 
 fun <E> Table<E>.json(name: String): Column<E, JsonNode> {
     return registerColumn(name, JsonSqlType)
+}
+
+// ObjectNode -> VARCHAR
+
+val JsonObjectSqlType =
+    JsonSqlType.transform(ObjectNode::class.java, { it as ObjectNode }, { it })
+
+fun <E> Table<E>.jsonObject(name: String): Column<E, ObjectNode> {
+    return registerColumn(name, JsonObjectSqlType)
 }

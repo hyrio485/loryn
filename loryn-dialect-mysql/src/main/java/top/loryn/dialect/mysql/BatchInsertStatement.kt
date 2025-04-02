6a61af372d13fce5836cf9d9a3af9544a421cae5
wangchen3 <wangchen3@eastmoney.com>
2025-04-02 08:51:56 +0800

    chore: 将mysql批量插入的方法名由batchInsert修改为insert

--- loryn-dialect-mysql/src/main/java/top/loryn/dialect/mysql/BatchInsertStatement.kt
+++ loryn-dialect-mysql/src/main/java/top/loryn/dialect/mysql/BatchInsertStatement.kt
@@ -32,7 +32,7 @@ class BatchInsertStatement<E>(
     }
 }
 
-fun <E, T : Table<E>> Database.batchInsert(
+fun <E, T : Table<E>> Database.insert(
     table: T,
     entities: List<E>,
     columns: List<Column<E, *>> = table.insertColumns,
@@ -57,9 +57,9 @@ fun <E, T : Table<E>> Database.batchInsert(
     }
 }
 
-fun <E, T : Table<E>> Database.batchInsert(
+fun <E, T : Table<E>> Database.insert(
     table: T,
     entities: List<E>,
     vararg columns: Column<E, *>,
     useGeneratedKeys: Boolean = false,
-) = batchInsert(table, entities, columns.toList(), useGeneratedKeys)
+) = insert(table, entities, columns.toList(), useGeneratedKeys)

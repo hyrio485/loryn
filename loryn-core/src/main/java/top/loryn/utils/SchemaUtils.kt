cb34dbdac5e5d2ca48f3c46526633de7b5f6ea0f
wangchen3 <wangchen3@eastmoney.com>
2025-04-24 19:01:02 +0800

    fix: Class<*>.boxed返回值不正确

--- loryn-core/src/main/java/top/loryn/utils/SchemaUtils.kt
+++ loryn-core/src/main/java/top/loryn/utils/SchemaUtils.kt
@@ -1,18 +1,31 @@
 package top.loryn.utils
 
 import top.loryn.expression.SqlParam
+import java.lang.Boolean as JavaLangBoolean
+import java.lang.Byte as JavaLangByte
+import java.lang.Character as JavaLangCharacter
+import java.lang.Double as JavaLangDouble
+import java.lang.Float as JavaLangFloat
+import java.lang.Integer as JavaLangInteger
+import java.lang.Long as JavaLangLong
+import java.lang.Short as JavaLangShort
 
 typealias SqlParamList = MutableList<SqlParam<*>>
 
-val Class<*>.boxed
-    get() = when (this) {
-        Boolean::class.javaPrimitiveType -> Boolean::class.java
-        Byte::class.javaPrimitiveType -> Byte::class.java
-        Char::class.javaPrimitiveType -> Char::class.java
-        Short::class.javaPrimitiveType -> Short::class.java
-        Int::class.javaPrimitiveType -> Int::class.java
-        Long::class.javaPrimitiveType -> Long::class.java
-        Float::class.javaPrimitiveType -> Float::class.java
-        Double::class.javaPrimitiveType -> Double::class.java
-        else -> this
+val Class<*>.boxed: Class<out Any>
+    get() {
+        return if (isPrimitive) {
+            when (name) {
+                "boolean" -> JavaLangBoolean::class.java
+                "char" -> JavaLangCharacter::class.java
+                "byte" -> JavaLangByte::class.java
+                "short" -> JavaLangShort::class.java
+                "int" -> JavaLangInteger::class.java
+                "float" -> JavaLangFloat::class.java
+                "long" -> JavaLangLong::class.java
+                "double" -> JavaLangDouble::class.java
+                "void" -> Void::class.java
+                else -> this
+            }
+        } else this
     }

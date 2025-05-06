e7c13a5c8c47c1a867c9524ede6bd1a0226de9c4
wangchen3 <wangchen3@eastmoney.com>
2025-05-06 10:37:18 +0800

    feat: 新增FIRST_VALUE及LAST_VALUE方法

--- loryn-dialect-mysql/src/main/java/top/loryn/dialect/mysql/MysqlFunctions.kt
+++ loryn-dialect-mysql/src/main/java/top/loryn/dialect/mysql/MysqlFunctions.kt
@@ -18,3 +18,18 @@ fun <T> Column<T>.max() =
 
 fun rowNumber(partitionBy: List<ColumnExpression<*>>, orderBy: List<OrderByExpression>) =
     WindowExpression(FunctionExpression("ROW_NUMBER", IntSqlType), partitionBy, orderBy)
+
+fun rowNumber(partitionBy: ColumnExpression<*>, vararg orderBy: OrderByExpression) =
+    WindowExpression(FunctionExpression("ROW_NUMBER", IntSqlType), listOf(partitionBy), orderBy.toList())
+
+fun <T> firstValue(expr: SqlExpression<T>, partitionBy: List<ColumnExpression<*>>, orderBy: List<OrderByExpression>) =
+    WindowExpression(FunctionExpression("FIRST_VALUE", expr.sqlType, expr), partitionBy, orderBy)
+
+fun <T> firstValue(expr: SqlExpression<T>, partitionBy: ColumnExpression<*>, vararg orderBy: OrderByExpression) =
+    WindowExpression(FunctionExpression("FIRST_VALUE", expr.sqlType, expr), listOf(partitionBy), orderBy.toList())
+
+fun <T> lastValue(expr: SqlExpression<T>, partitionBy: List<ColumnExpression<*>>, orderBy: List<OrderByExpression>) =
+    WindowExpression(FunctionExpression("LAST_VALUE", expr.sqlType, expr), partitionBy, orderBy)
+
+fun <T> lastValue(expr: SqlExpression<T>, partitionBy: ColumnExpression<*>, vararg orderBy: OrderByExpression) =
+    WindowExpression(FunctionExpression("LAST_VALUE", expr.sqlType, expr), listOf(partitionBy), orderBy.toList())
